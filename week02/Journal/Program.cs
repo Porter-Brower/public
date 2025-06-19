c7a77be1962880deb17df7fa53add530b3217712
Porter Brower <your.email@example.com>
2025-06-19 12:44:49 -0600

    display change Program

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,15 +1,27 @@
-public class Resume
-{
-    public string _name;
-    public List<Job> _jobs = new List<Job>();
+using System;
+using System.Collections.Generic;
 
-    public void Display()
+class Program
+{
+    static void Main(string[] args)
     {
-        Console.WriteLine($"Name: {_name}");
-        Console.WriteLine("Jobs:");
-        foreach (Job job in _jobs)
-        {
-            job.Display();
-        }
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
