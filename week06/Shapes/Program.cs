1212f5e09508bb2cf81713cd669ab767966427d1
Porter Brower <your.email@example.com>
2025-07-17 16:57:49 -0600

    Shapes and colors

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+                                                   // those are my favorite colors 
+        shapes.Add(new Square("Yellow", 3));
+        shapes.Add(new Rectangle("baby Blue", 4, 5));
+        shapes.Add(new Circle("military sand", 6));
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {Math.Round(area, 2)}.");
+        }
     }
 }
